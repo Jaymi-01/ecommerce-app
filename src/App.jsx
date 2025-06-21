c669ce9f6c4da730dfb6b6a93a5e7e92cf6d543e
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-06-21 19:40:11 +0100

    update 1 file and move 1 file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import Header from './components/Header'
 import Hero from './components/Hero'
+import Category from './components/Category'
+import Type from './components/Type'
+import Services from './components/Services'
+import Products from './components/Products'
+import Banner from './components/Banner'
+import Review from './components/Review'
+import Instagram from './components/Instagram'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <>
       <Header />
       <Hero />
+      <Category />
+      <Type />
+      <Services />
+      <Products />
+      <Banner />
+      <Review />
+      <Instagram />
+      <Footer />
     </>
   )
 }
